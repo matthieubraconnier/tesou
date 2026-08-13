532d3c68cc8518b7fe24fed6554852a0cdf740d3
matthieubraconnier <matthieubraconnier@gmail.com>
2026-08-13 13:50:03 +0200

    Connecte TesOu au backend Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tesou/app/tesou_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://pupsvkzbanpfnwssjvfq.supabase.co',
+    anonKey: 'sb_publishable_plGETjmTGoEtsgLJE6RV6w_Bky9mZQ1',
+  );
+
   runApp(const TesouApp());
 }
